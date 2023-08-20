57a2372f8b44274b3b739afee893442a017bc747
YosiaMarvelS <116961113+YosiaMarvelS@users.noreply.github.com>
2023-08-20 15:45:12 +0800

    Update login-script.js
    bikin jadi sesuai format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,38 +1,30 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
-/* membuat variabel untuk elemen view */
-const loginFormElement = document.querySelector('#loginForm');
-const inputEmailElement = document.querySelector('#inputEmail');
-const inputPasswordElement = document.querySelector('#inputPassword');
-
-/* var utk email sama pass yang benar */
-const expectedEmail = 'admin@dicoding.com';
-const expectedPassword = 'superpassword';
-
-/* nambah fungsi klik */
-loginFormElement.addEventListener('submit', function(event) {
-    event.preventDefault();
-    /* TODO 1 : Mendapatkan input email dan password pengguna dari form. */
-    /* ngambil nilai email sama pass */
-    const email = inputEmailElement.value;
-    const password = inputPasswordElement.value;
-  
-    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
-       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
-       Jika tidak, maka panggil fungsi showPopUp().
-     */
-
-    /* ngecek email sama pass udah bener atau blm */
-    if (email == expectedEmail && password == expectedPassword) {
-      /* pindah ke halaman home */
-      goToHome();
-    } else {
-      /* pindah ke halaman login */
-      goToLogin()
-    }
-});
+    /* Comment : Membuat variabel untuk setiap elemen view. */
+    const loginFormElement = document.querySelector('#loginForm');
+    const inputEmailElement = document.querySelector('#inputEmail');
+    const inputPasswordElement = document.querySelector('#inputPassword');
+     
+    /* Comment : Membuat variabel untuk menyimpan informasi email dan password. */
+    const expectedEmail = 'admin@dicoding.com';
+    const expectedPassword = 'superpassword';
+     
+    /* Comment :  Menambahkan aksi klik pada button. */
+    loginFormElement.addEventListener('submit', function(event) {
+      event.preventDefault();
+      
+      /* Comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
+      const email = inputEmailElement.value;
+      const password = inputPasswordElement.value;
+     
+      /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
+      if (email == expectedEmail && password == expectedPassword) {
+     
+        /* Comment : Jika sesuai maka program akan berpindah ke halaman home. */
+        goToHome();
+     
+      } else {
+     
+        /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
+        showPopUp();
+     
+      }
+    });
